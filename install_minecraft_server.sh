55750a59f5fbf338b103895e591b2c604fbbcff1
HippopoStar <leocabanes@wanadoo.fr>
2018-05-10 23:53:18 +0000

    Correction de "install_minecraft_server.sh"

--- install_minecraft_server.sh
+++ install_minecraft_server.sh
@@ -2,12 +2,12 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install oracle-java8-jdk
-sudo mkdir -p /home/minecraft
-sudo wget -P /home/minecraft/ https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-sudo java -jar /home/minecraft/BuildTools.jar --rev 1.12.2
-sudo java -Xms512M -Xmx1008M -jar /home/minecraft/spigot-1.12.2.jar nogui
-sudo sed -i 's/false/true/g' /home/minecraft/eula.txt
-echo "#!/bin/sh" > /home/minecraft/run-minecraft.sh
-echo "/usr/bin/java -Xms512M -Xmx1008M -jar /home/minecraft/spigot-1.12.2.jar nogui" >> /home/minecraft/run-minecraft.sh
-sudo chmod a+x /home/minecraft/run-minecraft.sh
-
+sudo mkdir -p /home/pi/minecraft
+sudo wget -P /home/pi/minecraft/ https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+sudo java -jar /home/pi/minecraft/BuildTools.jar --rev 1.12.2
+sudo java -Xms512M -Xmx1008M -jar /home/pi/minecraft/spigot-1.12.2.jar nogui
+sudo sed -i 's/false/true/g' /home/pi/minecraft/eula.txt
+sudo echo "#!/bin/sh" > /home/pi/minecraft/run-minecraft.sh
+sudo echo "/usr/bin/java -Xms512M -Xmx1008M -jar /home/pi/minecraft/spigot-1.12.2.jar nogui" >> /home/pi/minecraft/run-minecraft.sh
+sudo echo "iptables -A INPUT -p TCP --dport 25565 -j ACCEPT" >> /home/pi/minecraft/run-minecraft.sh
+sudo chmod a+x /home/pi/minecraft/run-minecraft.sh
