54c76ec2bd2662f6eff50df621bac33ae2b811c9
HippopoStar <leocabanes@wanadoo.fr>
2018-05-11 14:17:53 +0000

    Ajustements

--- iptables_openclassrooms.sh
+++ iptables_openclassrooms.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+#Ajouter la ligne suivante au fichier '/etc/rc.local' :
+#	/home/pi/iptables_openclassrooms.sh > /home/pi/iptables_openclassrooms.log 2>&1
+
 iptables -A INPUT -p tcp --dport ssh -j ACCEPT
 iptables -A INPUT -p tcp --dport www -j ACCEPT
 iptables -A INPUT -p tcp --dport imap2 -j ACCEPT
