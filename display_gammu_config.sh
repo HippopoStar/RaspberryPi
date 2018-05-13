85aa65673e994dd52fd914c3a5beeb5277d6ab51
HippopoStar <leocabanes@wanadoo.fr>
2018-05-13 00:11:09 +0000

    Ajustements "display_gammu_config.sh"

--- display_gammu_config.sh
+++ display_gammu_config.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
-echo "\033[32mdmesg | grep \"ttyUSB*\"\033[00m"
-dmesg | grep "ttyUSB*"
 echo "\033[32mcat /home/pi/.gammurc\033[00m"
 cat /home/pi/.gammurc
+echo "\033[32mcat /etc/gammurc\033[00m"
+cat /etc/gammurc
+echo "\033[32mcat /etc/gammu-smsdrc\033[00m"
+cat /etc/gammu-smsdrc
+echo "\033[32mdmesg | grep \"ttyUSB*\"\033[00m"
+dmesg | grep "ttyUSB*"
 echo "\033[32mgammu-detect\033[00m"
 gammu-detect
 echo "\033[32mgammu --identify\033[00m"
 gammu --identify
-echo "\033[32mcat /etc/gammu-smsdrc\033[00m"
-cat /etc/gammu-smsdrc
+echo "\033[32mgammu getsecuritystatus\033[00m"
+gammu getsecuritystatus
