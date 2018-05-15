93fc094ad7ed5fa31b5beff9c090262612d5e175
HippopoStar <leocabanes@wanadoo.fr>
2018-05-16 01:10:15 +0200

    Ebauche d'elaboration d'un script affichant les SMS recus

--- script_shells/kill_process.sh
+++ script_shells/kill_process.sh
@@ -11,7 +11,7 @@ else
 	ps aux | grep "PID\|${1}" | grep -v "kill_process.sh\|grep"
 	echo ""
 	read -p "Souhaitez-vous arreter ce(s) processus ? (Y/n) : " VALUE
-	if [ VALUE == 'Y' ] ; then
+	if [ \"$(VALUE)\" = "Y" ] ; then
 		kill $(ps aux | grep "${1}" | grep -v "kill_process.sh\|grep" | awk '{ print $2 }')
 		echo "Tache(s) arretee(s)"
 	else
