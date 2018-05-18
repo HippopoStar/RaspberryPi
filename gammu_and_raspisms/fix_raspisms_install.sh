e7930187d814f3f723110d8c39d606384c818186
HippopoStar <leocabanes@wanadoo.fr>
2018-05-18 05:08:43 +0200

    Il fallait degager "/var/www/html/RaspiSMS/receiveds/.to-keep"

--- gammu_and_raspisms/fix_raspisms_install.sh
+++ gammu_and_raspisms/fix_raspisms_install.sh
@@ -2,5 +2,7 @@
 
 sudo chown -R gammu:gammu /var/www/html/RaspiSMS/receiveds
 
+sudo mv /var/www/html/RaspiSMS/receiveds/.to-keep /var/www/html/RaspiSMS/
+
 sudo sed -i 's/\/var\/www\/RaspiSMS\/receiveds\//\/var\/www\/html\/RaspiSMS\/receiveds\//g' /var/www/html/RaspiSMS/parseSMS.sh
 
